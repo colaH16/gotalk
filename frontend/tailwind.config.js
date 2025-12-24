80267a00c973efc2e3ec8cb547c5b84206c77cc8
colaH16 <admin@cola16.dev>
2025-12-25 02:26:41 +0900

    gotalk: reduce margin of message, fix streaming

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  content: ["./static/**/*.html"], // HTML 파일 위치
+  // 현재 폴더(frontend) 아래의 모든 html, js 파일을 보겠다는 뜻
+  content: ["./**/*.{html,js}"], 
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["pastel"], // 테마 설정
+    themes: ["pastel"],
   },
 }
